ad1ea0220c3668aee685488acce374d7e2e9cf19
harsh-kumar-saaniya <harshvithal1@gmail.com>
2023-03-02 04:05:50 +0500

    React query template structure and simple project where data is being fetching from api through react query and displying it in a dom

--- react-query-project/src/pages/_app.tsx
+++ react-query-project/src/pages/_app.tsx
@@ -4,12 +4,10 @@ import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
 const queryClient = new QueryClient()
 
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
     </QueryClientProvider>
   )
-
 }
